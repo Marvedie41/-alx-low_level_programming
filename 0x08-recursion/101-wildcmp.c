77c41b7189be75373894aad6a3abd89914ff6fdc
Marvedie41 <eddiemugendi0413@gmail.com>
2023-04-06 17:11:01 -0700

    101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,33 @@
+/**
+ * wildcmp - Compares two strings 
+ * @s1: Pointer to the first string
+ * @s2: Pointer to the second string
+ *
+ * Return: 1 if the strings are identical, 0 if not
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (!s1 || !*s1)
+	{
+		if (s2 && *s2)
+		{
+			if (*s2 == '*')
+			{
+				return (wildcmp(s1, s2 + 1));
+			}
+			return (0);
+		}
+		return (1);
+	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
