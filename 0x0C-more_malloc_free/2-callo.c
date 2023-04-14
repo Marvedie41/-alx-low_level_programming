6e96d732f3792160dc9a7c4318b7f7386f32b8e2
Marvedie41 <eddiemugendi0413@gmail.com>
2023-04-13 19:11:41 -0700

    2-callo.c

---
+++ 0x0C-more_malloc_free/2-callo.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int x = 0;
+
+	while (x < n)
+	{
+		s[x] = b;
+		x++;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - allocates memory for an array using malloc
+ * @nmemb: n elements
+ * @size: bytes
+ * Return: pointer
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *pointer;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	pointer = malloc(nmemb * size);
+
+	if (pointer == NULL)
+		return (NULL);
+
+	_memset(pointer, 0, (nmemb * size));
+	return (pointer);
+}
