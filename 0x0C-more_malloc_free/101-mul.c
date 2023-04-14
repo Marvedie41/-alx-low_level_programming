57e3ad0f4f7ef38cfeaf676efe68c3faf90b54a7
Marvedie41 <eddiemugendi0413@gmail.com>
2023-04-14 01:15:19 -0700

    correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,17 +1,15 @@
 #include "main.h"
 
 /**
- * main - multiplies two positive numbers
- * @argc: number of arguments passed to the program
- * @argv: array of arguments passed to the program
- *
- * Return: 0 if successful, 98 if invalid number of arguments or non-positive
- * numbers were passed as arguments.
+ * main - Multiplies two positive numbers
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ * Return: 0 if successful, 98 if arguments are invalid
  */
 int main(int argc, char *argv[])
 {
-	unsigned long x;
-	int a, b;
+	unsigned long mul;
+	int i = 1, j;
 
 	if (argc != 3)
 	{
@@ -19,19 +17,24 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 
-	for (a = 1; a < argc; a++)
-	{
-		for (b = 0; argv[a][b] != '\0'; b++)
-		{
-			if (argv[a][b] > 57 || argv[a][b] < 48)
+	do {
+		j = 0;
+
+		do {
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
 				printf("Error\n");
 				exit(98);
 			}
-		}
-	}
 
-	x = atol(argv[1]) *  atol(argv[2]);
-	printf("%lu\n", x);
+			j++;
+		} while (argv[i][j] != '\0');
+
+		i++;
+	} while (i < argc);
+
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
+
 	return (0);
 }
