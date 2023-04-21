de0575f40be88b911f78655f2ec6a980991b368f
Marvedie41 <eddiemugendi0413@gmail.com>
2023-04-20 19:11:00 -0700

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,44 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - Prints any combination of characters, integers, floats, and strings
- * @format: A string containing the format of the arguments to be printed
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
  *
- * Return: No return value
+ * Return: no return.
  */
 void print_all(const char * const format, ...)
 {
-	va_list arg_list;
-	unsigned int i = 0, j, comma_flag = 0;
+	va_list args_list;
+	unsigned int i = 0, j, comma_needed = 0;
 	char *str;
-	const char type_arg[] = "cifs";  /* valid format specifiers */
+	const char valid_types[] = "cifs";
 
-	va_start(arg_list, format);
-
+	va_start(args_list, format);
 	while (format && format[i])
 	{
 		j = 0;
-		while (type_arg[j])
+		while (valid_types[j])
 		{
-			if (format[i] == type_arg[j] && comma_flag)
+			if (format[i] == valid_types[j] && comma_needed)
 			{
 				printf(", ");
 				break;
 			}
 			j++;
 		}
-
 		switch (format[i])
 		{
 		case 'c':
-			printf("%c", va_arg(arg_list, int));
-			comma_flag = 1;
+			printf("%c", va_arg(args_list, int)), comma_needed = 1;
 			break;
 		case 'i':
-			printf("%d", va_arg(arg_list, int));
-			comma_flag = 1;
+			printf("%d", va_arg(args_list, int)), comma_needed = 1;
 			break;
 		case 'f':
-			printf("%f", va_arg(arg_list, double));
-			comma_flag = 1;
+			printf("%f", va_arg(args_list, double)), comma_needed = 1;
 			break;
 		case 's':
-			str = va_arg(arg_list, char *);
-			comma_flag = 1;
+			str = va_arg(args_list, char *), comma_needed = 1;
 			if (!str)
 			{
 				printf("(nil)");
@@ -56,6 +50,5 @@ void print_all(const char * const format, ...)
 		i++;
 	}
 	printf("\n");
-
-	va_end(arg_list);
+	va_end(args_list);
 }
